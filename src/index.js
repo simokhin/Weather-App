888ffb30baf11136a66a71a5265c95fb97027474
simokhin <nick.simokhin@yandex.ru>
2024-10-16 00:48:30 +0300

    Add function to display weather and styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import "./styles.css";
 
 const apiKey = "HNA9QJ44F2YK8BRZ4CW6SLAY2";
 
-let city = `Moscow`;
-let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?unitGroup=metric&key=${apiKey}`;
+const searchField = document.querySelector("#search");
+searchField.addEventListener("change", (event) => {
+  event.preventDefault();
+  let city = searchField.value;
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?unitGroup=metric&key=${apiKey}`;
+  
+
+
+  dataProcessing(url);
+  return city;
+})
+
+
+
+
 
 async function weatherData(url) {
 	try {
@@ -17,8 +30,38 @@ async function weatherData(url) {
 	return weatherData;
 }
 
-async function dataProcessing() {
+async function dataProcessing(url) {
 	const data = await weatherData(url);
+
+  const grid = document.querySelector(".grid");
+  grid.classList.remove("none");
+
+  const city = document.querySelector("#city");
+  city.textContent = data.resolvedAddress;
+
+  const cloudCover = document.querySelector("#cloudcover");
+  cloudCover.textContent = data.currentConditions.cloudcover + "%";
+
+  const currentTemp = document.querySelector("#temp");
+  currentTemp.textContent = data.currentConditions.temp + "C";
+
+  const feelsLike = document.querySelector("#feelslike");
+  feelsLike.textContent = data.currentConditions.feelslike + "C";
+
+  const windSpeed = document.querySelector("#windspeed");
+  windSpeed.textContent = data.currentConditions.windspeed + " м/с";
+
+  const humidity = document.querySelector("#humidity");
+  humidity.textContent = data.currentConditions.humidity + "%"
+
+  const precip = document.querySelector("#precip");
+  precip.textContent = data.currentConditions.precipprob + "%";
+
+  const week = document.querySelector(".week");
+  //week.textContent = data.days[0].temp + "C";
+
 	console.log(data);
 }
+
+
 
