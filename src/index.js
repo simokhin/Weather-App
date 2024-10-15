a1231c77fe418bdd1a4f0490b7c192b976ab1fb2
simokhin <nick.simokhin@yandex.ru>
2024-10-15 22:44:11 +0300

    Add function to get weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import "./reset.css";
 import "./styles.css";
 
-alert("Test")
+const apiKey = "HNA9QJ44F2YK8BRZ4CW6SLAY2";
+
+let city = `Moscow`;
+let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/?unitGroup=metric&key=${apiKey}`;
+
+async function weatherData(url) {
+	try {
+		const response = await fetch(url, { mode: "cors" });
+		const weatherData = await response.json();
+		return weatherData;
+	} catch (err) {
+		console.log(err);
+	}
+	return weatherData;
+}
+
+async function dataProcessing() {
+	const data = await weatherData(url);
+	console.log(data);
+}
+
